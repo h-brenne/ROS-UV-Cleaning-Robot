a5617a175414832517671edeff77db29c5095014
Håvard Brenne <hava.brenne@gmail.com>
2022-01-30 11:00:21 +0100

    Fixed reset uv, publish RL maps for CNN

--- src/uvc_lamp/src/uvc_progress.cpp
+++ src/uvc_lamp/src/uvc_progress.cpp
@@ -2,9 +2,9 @@
 #include <grid_map_ros/grid_map_ros.hpp>
 #include <grid_map_msgs/GridMap.h>
 #include <cmath>
-//#include "std_msgs/MultiArrayLayout.h"
-//#include "std_msgs/MultiArrayDimension.h"
-//#include <std_msgs/ByteMultiArray.h>
+#include "std_msgs/MultiArrayLayout.h"
+#include "std_msgs/MultiArrayDimension.h"
+#include <std_msgs/Float32MultiArray.h>
 #include <std_msgs/Int32.h>
 
 bool first_run = true;
@@ -13,8 +13,8 @@ ros::Publisher coverage_publisher;
 
 void analyze_progress(grid_map::GridMap map, ros::Publisher pub){
   int covered_grids = 0;
-  //std_msgs::ByteMultiArray covered;
-  std_msgs::Int32 covered;
+  std_msgs::Float32MultiArray covered;
+  //std_msgs::Int32 covered;
 
   /*occ_grid.info.width = map.getSize().x();
   occ_grid.info.height = map.getSize().y();
@@ -27,9 +27,9 @@ void analyze_progress(grid_map::GridMap map, ros::Publisher pub){
   
 
   double coverage_threshold = 10.0; //mW 
-  grid_map::Matrix& energy_data = map["energy"];
+  
 
-  /*covered.layout.dim.push_back(std_msgs::MultiArrayDimension());
+  covered.layout.dim.push_back(std_msgs::MultiArrayDimension());
   covered.layout.dim.push_back(std_msgs::MultiArrayDimension());
 
   covered.layout.dim[0].label = "height";
@@ -39,30 +39,32 @@ void analyze_progress(grid_map::GridMap map, ros::Publisher pub){
   covered.layout.dim[0].stride = map.getSize().y()*map.getSize().x();
   covered.layout.dim[1].stride = map.getSize().x();
   covered.data.resize(map.getSize().y()*map.getSize().x());
-  covered.layout.data_offset = 0;*/
+  covered.layout.data_offset = 0;
   //std::vector<std::b> vec(W*H, 0);
 
   
   //Loop through all free indices
+  grid_map::Matrix& energy_data = map["energy"];
   for (std::set<uint>::iterator iterator=free_indices.begin(); iterator != free_indices.end(); ++iterator) {
       grid_map::Index index = grid_map::getIndexFromLinearIndex(*iterator, map.getSize());
       int linear_index = grid_map::getLinearIndexFromIndex(index, map.getSize(), true); //Row major for Occ map
       average_energy += energy_data(index(0),index(1))/(double(free_indices.size()));
-
+      
+      covered.data[linear_index]=energy_data(index(0),index(1));
+      
       if(energy_data(index(0),index(1)) > coverage_threshold){
             covered_grids++;
-            //covered.data[linear_index]= u_int8_t(1);
           }
     }
 
   covered_area = covered_grids*pow(map.getResolution(),2);
   percentage_covered = 100.0*covered_grids/free_indices.size();
 
-  covered.data=covered_grids;
+  //covered.data=covered_grids;
   pub.publish(covered);
 
   double total_area = free_indices.size()*pow(map.getResolution(),2);
-  ROS_INFO_THROTTLE(1,"uvc_progress: Covered: %f %%, %f m^2 of %f m^2. Average energy: %f mW"
+  ROS_INFO_THROTTLE(10,"uvc_progress: Covered: %f %%, %f m^2 of %f m^2. Average energy: %f mW"
   , percentage_covered, covered_area, total_area, average_energy);
 }
 
@@ -72,7 +74,7 @@ public:
   SubscribeAndPublish()
   {
     //Topic you want to publish
-    pub_ = n_.advertise<std_msgs::Int32>("covered", 10);;
+    pub_ = n_.advertise<std_msgs::Float32MultiArray>("covered", 10);;
 
     //Topic you want to subscribe
     sub_ = n_.subscribe("/uvc_grid_map/grid_map", 1, &SubscribeAndPublish::sub_cb, this);
@@ -85,7 +87,7 @@ public:
   
     if(first_run){
       first_run = false;
-      ROS_INFO("uvc_progress: Got map! Size: %d,%d", uvc_map.getSize().x(), uvc_map.getSize().x());
+      ROS_INFO("uvc_progress: Got map! Size: %d,%d", uvc_map.getSize().x(), uvc_map.getSize().y());
       //Find free grids to be iterated in subsequent runs
       grid_map::Matrix& occ_data = uvc_map["occupancy"];
       for (grid_map::GridMapIterator iterator(uvc_map); !iterator.isPastEnd(); ++iterator) {
@@ -94,6 +96,7 @@ public:
           free_indices.insert(grid_map::getLinearIndexFromIndex(index, uvc_map.getSize()));
         }
       }
+    ROS_INFO("Grids to cover: %ld", free_indices.size());
   }
   analyze_progress(uvc_map, pub_);
   }
